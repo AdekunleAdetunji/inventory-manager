da512bf1eaa640829d15018263ea5ff7ee642764
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-11-13 17:16:18 +0100

    feat: define product orm model

---
+++ main/database/models/product.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+This module contains the definition of the product orm model
+"""
+from main.database.models.basemodel import BaseModel
+from main.database.models.inventory import Inventory
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy import UUID
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+
+class Product(BaseModel):
+    """inventory product model"""
+
+    __tablename__ = "product"
+
+    name: Mapped[str] = mapped_column(String(30), nullable=False, unique=True)
+    sku: Mapped[str] = mapped_column(String(8), nullable=False, unique=True)
+    description: Mapped[str] = mapped_column(nullable=True)
+    price: Mapped[float] = mapped_column(nullable=False)
+    category_id: Mapped[UUID] = mapped_column(ForeignKey("category.id"))
+    category: Mapped["Category"] = relationship(back_populates="products")  # type: ignore
+    inventories: Mapped[list["Inventory"]] = relationship(
+        back_populates="product", cascade="all, delete-orphan"
+    )
