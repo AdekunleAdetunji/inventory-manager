2d6175aa102c8fce41a1ea06d0f69ab7ff54c090
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2024-11-13 17:14:38 +0100

    feat: define declarative base class

---
+++ main/database/base.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module contains the declarative base utility class
+"""
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    """Declarative base class for all database tables"""
+
+    pass
