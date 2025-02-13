af6bd65b187fd42d4df07b69f2330455ef7fd0c0
Adekunle Adetunji <adekunleadetunjiwilson@gmail.com>
2025-02-13 04:44:50 -0500

    test: create token pytest fixture

--- test/test_sub_apps/__init__.py
+++ test/test_sub_apps/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-This module contains shared objects between package modules
+This module contains shared objects between test_sub_apps package modules
 """
+import pytest
 from fastapi.testclient import TestClient
 from main.database.engine import db_session
 from main.sub_apps.admin import admin
@@ -23,3 +24,17 @@ admin.dependency_overrides[db_session] = override_get_db
 
 # Create test client
 client = TestClient(admin)
+
+
+@pytest.fixture()
+def token(admin_kwargs):
+    """fixture to create new admin and yield the login response the new admin"""
+    # register a new admin to the database
+    response = client.post("/new-admin", json=admin_kwargs)
+    assert response.status_code == 201
+    # login to obtain access token for new admin
+    login_response = client.post("/token", data=admin_kwargs)
+    assert login_response.status_code == 200
+
+    # yield token from login response
+    yield login_response.json()["access_token"]
